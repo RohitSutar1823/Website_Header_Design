16a180d891abdc0c5453cf713729ccb12f826935
Rohit Sutar <rohit1823sutar@gmail.com>
2026-09-02 18:52:54 +0530

    feat: add hero data and state management to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Background from './Components/Background/Background';
 
 const App = () => {
+  let heroData = [
+    {text1:"Dive into", text2: "what you love"},
+    {text1:"Indulge", text2: "your passions"},
+    {text1:"Give in to", text2: "your passions"},
+  ];
+
+  const [HeroCount, setHeroCount] = useState(2);
+  const [playStatus, setplayStatus] = useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} HeroCount={HeroCount}/>
     </div>
   )
 }
