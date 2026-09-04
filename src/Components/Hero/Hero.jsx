2e13ea68bca3d1e667d618fae1d75f7faaa4ec5e
Rohit Sutar <rohit1823sutar@gmail.com>
2026-09-04 09:53:25 +0530

    feat: add video play button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 import arrow_btn from '../../assets/arrow_btn.png'
 import play_icon from '../../assets/play_icon.png'
 import pause_icon from '../../assets/pause_icon.png'
-const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+const Hero = ({heroData,setHeroCount,heroCount,setplayStatus,playStatus}) => {
   return (
     <div className='hero'>
       <div className="hero-text">
@@ -21,8 +21,11 @@ const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
           <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot-orange":"hero-dot"}></li>
         </ul>
       </div>
+      <div className="hero-play">
+        <img onClick={()=>setplayStatus(!playStatus)} src={playStatus?pause_icon:play_icon} alt="" />
+      </div>
     </div>
   )
 }
 
-export default Hero
+export default Herofeat
